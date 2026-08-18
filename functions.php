9406174278f8476e560ea0ceefaf847051883fc0
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-08-18 11:54:02 -0300

    fazendo header

--- functions.php
+++ functions.php
@@ -3,6 +3,10 @@ function Menu($colecao){
     if (!empty($colecao)) {
         $menu = '<nav><ul>';
             foreach($colecao as $key => $value){
+                if ($value == " | ") {
+                    $menu .= '<li>'.$value.'</li>';
+                    continue; 
+                }
                 $menu .= '<li><a href="'.$key.'.php">'.$value.'</a></li>';
             }
         $menu .= '</ul></nav>';
--- index.php
+++ index.php
@@ -4,23 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Site<title>
+    <title>Fazendo site com PHP</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <?php 
             $colecaoMenu = array(
-                'index'=>'Home ',
-                'marketing'=>'| Marketing ',
-                'internet'=>'| Internet ',
-                'dinheiro'=>'| Ganhar Dinheiro ',
-                'webmaster'=>'| Webmaster ',
-                'scripts'=>'| Scripts ',
-                'software'=>'| Software ',
-                'comercio'=>'| Comércio Eletrônico ',
-                'downloads'=>'| Downloads ',
-                'contato'=>'| Contato',
+                'index'=>'Home',
+                " | ",
+                'marketing'=>'Marketing',
+                " | ",
+                'internet'=>'Internet',
+                " | ",
+                'dinheiro'=>'Ganhar Dinheiro',
+                " | ",
+                'webmaster'=>'Webmaster',
+                " | ",
+                'scripts'=>'Scripts',
+                " | ",
+                'software'=>'Software',
+                " | ",
+                'comercio'=>'Comércio Eletrônico',
+                " | ",
+                'downloads'=>'Downloads',
+                " | ",
+                'contato'=>'Contato',
             );
             Menu($colecaoMenu,"SitePHP");
         ?>
