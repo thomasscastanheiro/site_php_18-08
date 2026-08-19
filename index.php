ea119ba26642f9f6e64c55bf32ba81305b1d1e7d
Thomas Castanheiro <thomascastanheiro2010@gmail.com>
2026-08-19 09:20:52 -0300

    fazendo section

--- index.php
+++ index.php
@@ -35,7 +35,33 @@
         ?>
     </header>
     <main>
-        
+        <?php $colecaoNoticias = array(
+            array(
+                'titulo'=>'Niclas Eliasson é o novo reforço!',
+                'descricao'=>'',
+                'imagem'=>'artigo.png',
+                'texto'=>'Atacante sueco de 30 anos é contratado pelo Sport Club Internacional!'
+            ),
+            array(
+                'titulo'=>'',
+                'descricao'=>'',
+                'imagem'=>'artigo.png',
+                'texto'=>''
+            ),
+            array(
+                'titulo'=>'',
+                'descricao'=>'',
+                'imagem'=>'artigo.png',
+                'texto'=>''
+                )
+            );?>
+        <section>
+            <h1>DESTAQUES</h1>
+            <img src="<?php echo $colecaoNoticias[0]['imagem']?>" alt="">
+            <h2><?php echo $colecaoNoticias[0]['titulo'];?></h2>
+            <h3><?php echo $colecaoNoticias[0]['texto']?></h3>
+            <button><a href="<?php echo $colecaoNoticias[0]['titulo']?>.php"></a></button>
+        </section>
     </main>
 </body>
 </html>
